0d95b01bfded657fc76b7b05177661f17081ef88
lapin008800 <yui.lapin0419@gmail.com>
2022-05-08 21:58:22 +0900

    【修正】4章_課題提出

---
+++ 4-1/src/check/Main.java
@@ -0,0 +1,33 @@
+package check;
+import constants.Constants;
+
+//課題①
+public class Main {
+
+	  	
+	    private static String firstName = "ゆい";		//名前
+	    private static String lastName = "はしづめ";	//名字
+
+	  
+//課題②
+	 
+	  private static  String printName(String firstName, String lastName) {
+	    return lastName + firstName;
+	  }
+
+	  
+//課題③
+	  public static void main(String[] args) {
+
+		    System.out.println("printNameメソッド　→　" + printName(firstName, lastName));
+
+	  	
+	Pet pt = new Pet(Constants.CHECK_CLASS_JAVA, Constants.CHECK_CLASS_HOGE);		
+	RobotPet rpt = new RobotPet(Constants.CHECK_CLASS_R2D2, Constants.CHECK_CLASS_LUKE);
+
+	pt.introduce();
+	rpt.introduce();
+
+	
+	  }
+}
